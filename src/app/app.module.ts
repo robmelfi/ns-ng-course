c099477c7602c89ee848a0a3abce9af6839d27f7
robmelfi <melfiroberto@gmail.com>
2020-04-19 02:49:17 +0200

    add flexbox playground

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CurrentChallengeComponent } from "./challenges/current-challenge/curren
 // Uncomment and add to NgModule imports if you need to use two-way binding
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { StackComponent } from './layouts/stack/stack.component';
+import { FlexboxComponent } from './layouts/flexbox/flexbox.component';
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
@@ -21,7 +22,8 @@ import { StackComponent } from './layouts/stack/stack.component';
     declarations: [
         AppComponent,
         CurrentChallengeComponent,
-        StackComponent
+        StackComponent,
+        FlexboxComponent
     ],
     providers: [],
     schemas: [
