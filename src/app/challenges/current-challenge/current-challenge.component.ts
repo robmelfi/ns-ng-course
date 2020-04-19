0ae30b2312319a214c9e9ea292ed2b2b512cd947
robmelfi <melfiroberto@gmail.com>
2020-04-19 18:40:22 +0200

    Create a shared component for back button

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,9 +1,5 @@
 import { Component } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
-import { isAndroid } from 'platform';
-import { Page } from "tns-core-modules/ui/page/page";
-
-declare var android: any;
 
 @Component({
     selector: 'ns-current-challenge',
@@ -12,24 +8,9 @@ declare var android: any;
     moduleId: module.id
 })
 export class CurrentChallengeComponent {
-    constructor(private router: RouterExtensions, private page: Page) {}
+    constructor(private router: RouterExtensions) {}
 
     onEdit() {
         this.router.navigate(['/challenge-edit'])
-    }
-
-    onLoadedActionBar() {
-        if (isAndroid) {
-            console.log('---->  android')
-            const androidToolbar = this.page.actionBar.nativeView;
-            const backButton = androidToolbar.getNavigationIcon();
-            if (backButton) {
-                backButton.setColorFilter(
-                    android.graphics.Color.parseColor('#171717'),
-                    (<any>android.graphics).PorterDuff.Mode.SRC_ATOP
-                );
-            }
-
-        }
     }
 }
