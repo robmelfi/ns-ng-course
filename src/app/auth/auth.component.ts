0e2b8257def920c277f988acba535c4fd956c6a7
robmelfi <melfiroberto@gmail.com>
2020-04-19 16:54:25 +0200

    Implementing page navigation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ns-auth',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSignin() {
+      this.router.navigate(['/today']);
   }
 
 }
