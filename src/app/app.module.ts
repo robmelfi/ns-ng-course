b1be27d1c0a66872f637530cffc738b03d18a528
robmelfi <melfiroberto@gmail.com>
2020-04-20 15:13:35 +0200

    add a sidedrawer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TodayComponent } from './challenges/today/today.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { ActionBarComponent } from './shared/ui/action-bar/action-bar.component';
 import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-tabs.component';
+import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives'
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
@@ -21,7 +22,10 @@ import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-ta
         AppComponent
     ],
     imports: [
-        NativeScriptModule, NativeScriptFormsModule, AppRoutingModule
+        NativeScriptModule,
+        NativeScriptFormsModule,
+        NativeScriptUISideDrawerModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
