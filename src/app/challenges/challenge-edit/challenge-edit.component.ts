2613fca6c229cec8d8fee2421a3af1816a450177
robmelfi <melfiroberto@gmail.com>
2020-04-20 23:07:57 +0200

    read dynamic parameter

--- src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PageRoute } from 'nativescript-angular/router';
 
 @Component({
   selector: 'ns-challenge-edit',
   templateUrl: './challenge-edit.component.html',
   styleUrls: ['./challenge-edit.component.css']
 })
-export class ChallengeEditComponent  { }
+export class ChallengeEditComponent implements OnInit {
+    constructor(private activatedRoute: ActivatedRoute, private pageRoute: PageRoute) {}
+
+    ngOnInit() {
+        // this.activatedRoute.paramMap.subscribe( paramMap => {
+
+        // });
+        this.pageRoute.activatedRoute.subscribe(activatedRoute => {
+            activatedRoute.paramMap.subscribe(paramMap => {
+                console.log(paramMap.get('mode'));
+            });
+        });
+    }
+ }
--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -11,7 +11,7 @@ export class CurrentChallengeComponent {
     constructor(private router: RouterExtensions) {}
 
     onEdit() {
-        this.router.navigate(['/challenge-edit'], {
+        this.router.navigate(['/challenges/edit'], {
             transition: {name: 'slideLeft'}
         });
     }
