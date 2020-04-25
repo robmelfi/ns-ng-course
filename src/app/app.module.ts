796fe61a71ce230d2d8941a89742b1b363407d80
robmelfi <melfiroberto@gmail.com>
2020-04-25 23:28:37 +0200

    set up the reactive forms, add validation and reading values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives';
+import { ReactiveFormsModule } from "@angular/forms";
 import { DayModalComponent } from './challenges/day-modal/day-modal.component'
 import { SharedModule } from "./shared/ui/action-bar/shared.module";
 import { ChallengeActionsModule } from "./challenges/challenge-actions/challenge-actions.module";
@@ -23,6 +24,7 @@ import { ChallengeActionsModule } from "./challenges/challenge-actions/challenge
         NativeScriptModule,
         NativeScriptFormsModule,
         NativeScriptUISideDrawerModule,
+        ReactiveFormsModule,
         AppRoutingModule,
         SharedModule,
         ChallengeActionsModule
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { RouterExtensions } from 'nativescript-angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TextField } from 'tns-core-modules/ui/text-field';
 
 @Component({
-  selector: 'ns-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+	selector: 'ns-auth',
+	templateUrl: './auth.component.html',
+	styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+	form: FormGroup;
+	emailControlIsValid = true;
+	passwordControlIsValid = true;
+	@ViewChild('passwordEl', { static: false }) passwordEl: ElementRef<TextField>;
+	@ViewChild('emailEl', { static: false }) emailEl: ElementRef<TextField>;
 
-  ngOnInit(): void { }
+	constructor(private router: RouterExtensions) { }
+
+	ngOnInit(): void {
+		this.form = new FormGroup({
+			email: new FormControl(null, {
+				updateOn: 'blur',
+				validators: [Validators.required, Validators.email]
+			}),
+			password: new FormControl(null, {
+				updateOn: 'blur',
+				validators: [Validators.required, Validators.minLength(6)]
+			})
+		});
+	}
+
+	onSignin() {
+		this.router.navigate(['/today'], { clearHistory: true });
+	}
+
+	onSubmit() {
+		this.emailEl.nativeElement.focus();
+		this.passwordEl.nativeElement.focus();
+		this.passwordEl.nativeElement.dismissSoftInput();
+
+		const email = this.form.get('email').value;
+		const password = this.form.get('password').value;
+		console.log(email, password);
+	}
 }
