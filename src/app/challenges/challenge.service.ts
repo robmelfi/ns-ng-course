4396acb9365a28ec3c24733ee6cdf3cbca46970a
robmelfi <melfiroberto@gmail.com>
2020-04-26 16:07:59 +0200

    create new challenge in challenge service

--- src/app/challenges/challenge.service.ts
+++ src/app/challenges/challenge.service.ts
@@ -5,6 +5,20 @@ import { Challenge } from './challenge.model';
 
 @Injectable({ providedIn: 'root' })
 export class ChallengeService {
-  private _currentChallenge = new BehaviorSubject<Challenge>(null);
+	private _currentChallenge = new BehaviorSubject<Challenge>(null);
 
+	get currentChallenge() {
+		return this._currentChallenge.asObservable();
+	}
+
+	createNewChallenge(title: string, description: string) {
+		const newChallenge = new Challenge(
+			title,
+			description,
+			new Date().getFullYear(),
+			new Date().getMonth()
+		);
+		// Save it to server
+		this._currentChallenge.next(newChallenge);
+	}
 }
