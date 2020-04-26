400aea4a1bdfdbbcc1b8545bda4b532c41806923
robmelfi <melfiroberto@gmail.com>
2020-04-26 12:23:06 +0200

    switch between login and signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,57 +4,73 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { TextField } from 'tns-core-modules/ui/text-field';
 
 @Component({
-	selector: 'ns-auth',
-	templateUrl: './auth.component.html',
-	styleUrls: ['./auth.component.css']
+    selector: 'ns-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-	form: FormGroup;
-	emailControlIsValid = true;
-	passwordControlIsValid = true;
-	@ViewChild('passwordEl', { static: false }) passwordEl: ElementRef<TextField>;
-	@ViewChild('emailEl', { static: false }) emailEl: ElementRef<TextField>;
+    form: FormGroup;
+    emailControlIsValid = true;
+    passwordControlIsValid = true;
+    isLogin = true;
+    @ViewChild('passwordEl', { static: false }) passwordEl: ElementRef<TextField>;
+    @ViewChild('emailEl', { static: false }) emailEl: ElementRef<TextField>;
 
-	constructor(private router: RouterExtensions) { }
+    constructor(private router: RouterExtensions) { }
 
-	ngOnInit(): void {
-		this.form = new FormGroup({
-			email: new FormControl(null, {
-				updateOn: 'blur',
-				validators: [Validators.required, Validators.email]
-			}),
-			password: new FormControl(null, {
-				updateOn: 'blur',
-				validators: [Validators.required, Validators.minLength(6)]
-			})
-		});
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            email: new FormControl(null, {
+                updateOn: 'blur',
+                validators: [Validators.required, Validators.email]
+            }),
+            password: new FormControl(null, {
+                updateOn: 'blur',
+                validators: [Validators.required, Validators.minLength(6)]
+            })
+        });
 
-		this.form.get('email').statusChanges.subscribe(status => {
-			this.emailControlIsValid = status === 'VALID';
-		});
+        this.form.get('email').statusChanges.subscribe(status => {
+            this.emailControlIsValid = status === 'VALID';
+        });
 
-		this.form.get('password').statusChanges.subscribe(status => {
-			this.passwordControlIsValid = status === 'VALID';
-		});
-	}
+        this.form.get('password').statusChanges.subscribe(status => {
+            this.passwordControlIsValid = status === 'VALID';
+        });
+    }
 
-	onSignin() {
-		this.router.navigate(['/today'], { clearHistory: true });
-	}
+    onSignin() {
+        this.router.navigate(['/today'], { clearHistory: true });
+    }
 
-	onSubmit() {
-		this.emailEl.nativeElement.focus();
-		this.passwordEl.nativeElement.focus();
-		this.passwordEl.nativeElement.dismissSoftInput();
+    onSubmit() {
+        this.emailEl.nativeElement.focus();
+        this.passwordEl.nativeElement.focus();
+        this.passwordEl.nativeElement.dismissSoftInput();
 
-		const email = this.form.get('email').value;
-		const password = this.form.get('password').value;
-		console.log(email, password);
+        if (!this.form.valid) {
+            return;
+        }
+
+        const email = this.form.get('email').value;
+        const password = this.form.get('password').value;
+        this.form.reset();
+        this.emailControlIsValid = true;
+        this.passwordControlIsValid = true;
+        if (this.isLogin) {
+            console.log('Logging in...');
+        } else {
+            console.log('Signing up ...');
+        }
     }
 
     onDone() {
         this.emailEl.nativeElement.focus();
         this.passwordEl.nativeElement.focus();
         this.passwordEl.nativeElement.dismissSoftInput();
-      }
+    }
+
+    onSwitch() {
+        this.isLogin = !this.isLogin;
+    }
 }
