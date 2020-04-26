941f075b0a390c80f3328145e89a42bd8acd95eb
robmelfi <melfiroberto@gmail.com>
2020-04-26 19:17:46 +0200

    work on today page and state

--- src/app/challenges/today/today.component.ts
+++ src/app/challenges/today/today.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Day } from '../day.model';
+import { Subscription } from 'rxjs';
+import { ChallengeService } from '../challenge.service';
 
 @Component({
-  selector: 'ns-today',
-  templateUrl: './today.component.html',
-  styleUrls: ['./today.component.scss']
+    selector: 'ns-today',
+    templateUrl: './today.component.html',
+    styleUrls: ['./today.component.scss']
 })
 export class TodayComponent implements OnInit {
+    currentDay: Day;
+    private curChallengeSub: Subscription;
 
-  constructor() { }
+    constructor(private challengeService: ChallengeService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.curChallengeSub = this.challengeService.currentChallenge.subscribe(
+            challenge => {
+                if (challenge) {
+                    this.currentDay = challenge.currentDay;
+                }
+            }
+        );
+    }
 
-  onActionSelected(action: 'complete' | 'fail' | 'cancel') {
-    console.log(action)
-  }
+    onActionSelected(action: 'complete' | 'fail' | 'cancel') {
+        console.log(action)
+    }
+
+    ngOnDestroy() {
+        if (this.curChallengeSub) {
+            this.curChallengeSub.unsubscribe();
+        }
+    }
 
 }
